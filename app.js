737c29eb91c76ddf69c74fc55ce09085cd5b0e01
miikEL <Mikael-diep@hotmail.com>
2017-05-09 16:25:34 +0200

    Responsive på huvudmenyn Done

--- app.js
+++ app.js
@@ -1,28 +1,74 @@
+/* -------------------------- HOVER PICTURES ------------------- */
+
+//ProfilePictureColor
+$("#profilePictures").hover(function() {
+    
+    $("#profilePictureColor").fadeIn(0);
+    $("#profilePicture").hide(0);
+    
+}, function() {
+   
+    $("#profilePicture").show(0);
+    $("#profilePictureColor").fadeOut(0);
+    
+});
+
+//resumePictureColor
+$("#resumePictures").hover( function() {
+    
+    $("#resumePictureColor").fadeIn(0);
+    $("#resumePicture").hide(0);
+    
+}, function() {
+   
+    $("#resumePictureColor").fadeOut(0);
+    $("#resumePicture").show(0);
+    
+});
+
+//portfolioPictureColor
+$("#portfolioPictures").hover( function() {
+    
+    $("#portfolioPictureColor").fadeIn(0);
+    $("#portfolioPicture").hide(0);
+    
+}, function() {
+   
+    $("#portfolioPictureColor").fadeOut(0);
+    $("#portfolioPicture").show(0);
+    
+});
+
+//contactPictureColor
+$("#contactPictures").hover( function() {
+    
+    $("#contactPictureColor").fadeIn(0);
+    $("#contactPicture").hide(0);
+    
+}, function() {
+   
+    $("#contactPictureColor").fadeOut(0);
+    $("#contactPicture").show(0);
+    
+});
+
+/* ----------------------------------------------------------- */
+
 function slideLeft(pic, picColor, text, smalltext, icon) {
-    $(pic).animate({left:"100%"}, 500);
-    $(picColor).animate({left:"100%"}, 500);
-    $(text).animate({left:"100%"}, 500);
-    $(smalltext).animate({left:"100%"}, 500);
-    $(icon).animate({left:"100%"}, 500);
+    $(pic).animate({left:"150%"}, 500);
+    $(picColor).animate({left:"150%"}, 500);
+    $(text).animate({left:"150%"}, 500);
+    $(smalltext).animate({left:"150%"}, 500);
+    $(icon).animate({left:"150%"}, 500);
   
 }
 
 function slideRight(pic, picColor, text, textV2) {
-    $(pic).animate({right:"100%"});
-    $(picColor).animate({right:"100%"});
-    $(text).animate({left:"-100%"});
-    $(textV2).animate({left:"-100%"});
+    $(pic).animate({left:"-150%"});
+    $(picColor).animate({left:"-150%"});
+    $(text).animate({left:"-150%"});
+    $(textV2).animate({left:"-150%"});
 }
-
-
-//ProfilePicture
-$("#profilePictures").hover(function() {
-    
-    $("#profilePictureColor").fadeIn(300);
-}, function() {
-    
-    $("#profilePictureColor").fadeOut(300);
-});
 
 $("#profilePictures").click(function() {
     
@@ -102,31 +148,6 @@ $("#contactPictures").click(function() {
     
 })
 
-//ResumePicture
-$("#resumePictures").hover(function() {
-    
-    $("#resumePictureColor").fadeIn(300);
-}, function() {
-    
-    $("#resumePictureColor").fadeOut(300);
-});
 
-//PortfolioPicture
-$("#portfolioPictures").hover(function() {
-    
-    $("#portfolioPictureColor").fadeIn(300);
-}, function() {
-    
-    $("#portfolioPictureColor").fadeOut(300);
-});
-
-//ContactPicture
-$("#contactPictures").hover(function() {
-    
-    $("#contactPictureColor").fadeIn(300);
-}, function() {
-    
-    $("#contactPictureColor").fadeOut(300);
-});
 
 
